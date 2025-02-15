657e804debacce5fbaa2988bd819f5b23e1863a9
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2025-02-15 12:16:59 -0300

    utilizando .map

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,15 +2,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import { useState, useEffect } from "react"; 
 import '../styles/repositories.scss';
 
-
-// https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-};
-
 export function RepositoryList(){
 
   const [repositories,setRepositories] = useState([]);
@@ -24,11 +15,9 @@ export function RepositoryList(){
   <section className="repository-list">
     <h1>Lista de Repositórios</h1>
     <ul>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
-      <RepositoryItem repository={repository}/>
+      {repositories.map(repository => {
+        return  <RepositoryItem key={repository.name}repository={repository}/>
+      })}
     </ul>
   </section>
  );
